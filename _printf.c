debe6b634003a57e47cb32148b68572020ad2609
Chipo Mazhani <chipomazhani@gmail.com>
2023-11-13 14:40:45 -0800

    added conditions to printf function

--- _printf.c
+++ _printf.c
@@ -11,14 +11,23 @@
  */
 
 int _printf(const char *format, ...)
-{	
-
+{
 va_list ap;
 int count;
-
-va_start(ap; format);
-count =0;
-
+va_start(ap, format);
+count = 0;
+for (count = 0; format[count] != '\0'; count++)
+{
+if (format[count] == '%')
+{
+format++;
+char next_char = *format;
+print_format(next_char, ap)
 }
-
-
+else
+count += write(1, format, 1);
+++format;
+}
+va_end(ap);
+return (count);
+}
